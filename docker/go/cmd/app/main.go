e40ad39ea8770842dee705cb40efd697b41aee9e
koshiterada <koshi.terada@o-kun.com>
2022-03-11 00:47:18 +0900

    go.modとgo.sumの変更

--- docker/go/cmd/app/main.go
+++ docker/go/cmd/app/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"net/http"
+	"sample-docker-gin/internal/apps/app/mysql"
+)
+
+type TableSample struct {
+	Id   int    `gorm:"column:id"`
+	Name string `gorm:"column:name"`
+}
 
 func main() {
-    fmt.Println("Hello, 世界")
+
+	engine := gin.Default()
+	engine.GET("/", func(c *gin.Context) {
+
+		// MySQLとの接続
+		db := mysql.GormConnect()
+		defer db.Close()
+
+		//データ取得
+		var tableSample []TableSample
+		db.Table("table_sample").Find(&tableSample)
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "success!",
+			"data":    tableSample,
+		})
+	})
+	engine.Run(":3000")
 }
